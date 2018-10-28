3a6f067a091f0da68f58b44d5552b591a29e4f8b
hayzey <hayzeety@gmail.com>
2018-10-28 11:33:59 +0700

    Theming navbar

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -8,25 +9,30 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     imports: [
         CommonModule,
+        FlexLayoutModule,
         BrowserAnimationsModule,
         MatButtonModule,
         MatCardModule,
         MatFormFieldModule,
         MatInputModule,
         MatProgressSpinnerModule,
-        MatSelectModule
+        MatSelectModule,
+        MatToolbarModule
     ],
     exports: [
+        FlexLayoutModule,
         MatButtonModule,
         MatCardModule,
         MatFormFieldModule,
         MatInputModule,
         MatProgressSpinnerModule,
-        MatSelectModule
+        MatSelectModule,
+        MatToolbarModule
     ],
     declarations: []
 })
